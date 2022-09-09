ff719fb1e0bb5d9a9d8d088748905edc28011ed5
Yana Markos <yn.mrks@gmail.com>
2022-09-09 13:24:39 +0200

    Added exercise 5

---
+++ lesson_05/05_exercise.py
@@ -0,0 +1,9 @@
+def leap_year(year):
+    if year % 100 == 0 and year % 400 == 0:
+        return True
+    elif year % 100 !=0 and year % 4 == 0:
+        return True
+    else:
+        return False
+
+print(leap_year(2004))
